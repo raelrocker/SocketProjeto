5276fe67fd3181bd0957a6c541f95b8cae96d3a9
Israel Jardim <israjardim21@gmail.com>
2016-06-12 13:39:21 -0300

    Primeiro commit

---
+++ src/socketprojeto/SocketProjeto.java
@@ -0,0 +1,14 @@
+package socketprojeto;
+
+import Interfaces.frmPrincipal;
+
+public class SocketProjeto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frmPrincipal().setVisible(true);
+    }
+    
+}
